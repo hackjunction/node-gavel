edcba854856cb1b1e6d0387fe77ee194f52a2efb
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-06-27 16:03:09 +0300

    Finish Annotator schema

--- api/models/Annotator.js
+++ api/models/Annotator.js
@@ -7,8 +7,39 @@ var AnnotatorSchema = new Schema({
 		type: String,
 		required: true,
 	},
+	email: {
+		type: String,
+		required: true
+	},
+	active: {
+		type: Boolean,
+		default: true
+	},
+	read_welcome: {
+		type: Boolean,
+		default: false
+	},
+	secret: {
+		type: String,
+		required: true
+	},
+	next: {
+		type: Schema.Types.ObjectId,
+		ref: 'Item'
+	},
+	updated: {
+		type: Date
+	},
+	prev: {
+		type: Schema.Types.ObjectId,
+		ref: 'Item'
+	},
+	ignore_ids: [String],
+	alpha: Number,
+	beta: Number
 });
 
+/* Validate name */
 schema.path('name').validate(function (value) {
 	if (value.length > 120) {
 		return false;
