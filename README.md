f4c2fbf34c591a5a5f4d4b5deb82505996739fdb
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-06-27 15:25:33 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+**Install dependencies**
+Run `npm install`. Only needs to be run again whenever dependencies change.
+
+**Starting the development server**
+
+Run `npm start`, this will start the development server at localhost:3000. The development server will automatically reload when any code changes occur.
--- api/models/EXAMPLE.js
+++ api/models/EXAMPLE.js
@@ -4,21 +4,21 @@ var Schema = mongoose.Schema;
 
 
 var ExampleSchema = new Schema({
-	name: {
-		type: String,
-		required: 'Kindly enter the name of the task'
-	},
-	Created_date: {
-		type: Date,
-		default: Date.now
-	},
-	status: {
-		type: [{
-			type: String,
-			enum: ['pending', 'ongoing', 'completed']
-		}],
-		default: ['pending']
-	}
+    name: {
+        type: String,
+        required: 'Kindly enter the name of the task'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+            type: String,
+            enum: ['pending', 'ongoing', 'completed']
+        }],
+        default: ['pending']
+    }
 });
 
 module.exports = mongoose.model('Example', ExampleSchema);
