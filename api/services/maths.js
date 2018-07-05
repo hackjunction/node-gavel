d76942162ebc13e263de30f376aa409893a5ca48
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-07-05 15:13:21 +0300

    QUICK MATHS finished

--- api/services/maths.js
+++ api/services/maths.js
@@ -1,6 +1,17 @@
 const { exp, log } = require('numjs');
 const { betaln, psi } = require('scipy');
 
+//Should be equivalent to scipy.special.psi
+const digamma = require('@stdlib/math/base/special/digamma');
+
+//Should be equivalent to scipy.special.betaln
+const betaln = require('@stdlib/math/base/special/betaln');
+
+//Should be equivalent to numpy.exp
+const exp = require('@stdlib/math/base/special/exp');
+
+//Should be equivalent to numpy.log
+const log = require('@stdlib/math/base/special/ln');
 
 const GAMMA = 0.1;
 const LAMBDA = 1.0;
@@ -12,26 +23,11 @@ const BETA_PRIOR = 1.0;
 const EPSILON = 0.25;
 
 
-/**
- * This should be equivalent to scipy.special.betaln
- * https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.special.betaln.html
- */
-function betaln(a, b) {
-	//TODO: Implement this
-}
-
-/** 
- * Absolutely no clue how to do this, but should be equivalent to scipy.special.psi
- * https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.psi.html
-*/
-function psi() {
-	//TODO: Implement this
-}
-
-
+// Some QUICK MATHS
 // Port of gavel/crowd_bt.py in original Gavel. 
+//TODO: I would NOT be surprised if there is an error in here somewhere, needs to be thoroughly tested
 
-const QuickMATHS = {
+const Maths = {
 
 	argmax: (f, xs) => {
 		return Math.max(xs, f);
@@ -48,30 +44,120 @@ const QuickMATHS = {
 	},
 
 	divergenceBeta: (alpha_1, beta_1, alpha_2, beta_2) => {
-		//TODO: Implement this
+		return (
+			betaln(alpha_2, beta_2) - betaln(alpha_1, beta_1)
+			+ (alpha_1 - alpha_2) * digamma(alpha_1)
+			+ (beta_1 - beta_2) * digamma(beta_1)
+			+ (alpha_2 - alpha_1 + beta_2 - beta_1) * digamma(alpha_1 + beta_1)
+		);
 	},
 
 	update: (alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser) => {
-		//TODO: Implement this
+		const updated_annotator = Maths.updatedAnnotator(alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser);
+		const updated_mus = Maths.updatedMus(alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser);
+		const updated_sigma_sqs = Maths.updatedSigmaSqs(alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser);
+
+		return {
+			updated_alpha: updated_annotator.updated_alpha,
+			updated_beta: updated_annotator.updated_beta,
+			updated_mu_winner: updated_mus.updated_mu_winner,
+			updated_mu_loser: updated_mus.updated_mu_loser,
+			updated_sigma_sq_winner: updated_sigma_sqs.updated_sigma_sq_winner,
+			updated_sigma_sq_loser: updated_sigma_sqs.updated_sigma_sq_loser
+		}
 	},
 
 	expectedInformationGain: (alpha, beta, mu_a, sigma_sq_a, mu_b, sigma_sq_b) => {
-		//TODO: Implement this
+		const updated_annotator = updatedAnnotator(alpha, beta, mu_a, sigma_sq_a, mu_b, sigma_sq_b);
+		const updated_mus = updatedMus(alpha, beta, mu_a, sigma_sq_a, mu_b, sigma_sq_b);
+		const updated_sigma_sqs = updatedSigmaSqs(alpha, beta, mu_a, sigma_sq_a, mu_b, sigma_sq_b);
+		const updated_annotator_2 = updatedAnnotator(alpha, beta, mu_b, sigma_sq_b, mu_a, sigma_sq_a);
+		const updated_mus_2 = updatedMus(alpha, beta, mu_b, sigma_sq_b, mu_a, sigma_sq_a);
+		const updated_sigma_sqs_2 = updatedSigmaSqs(alpha, beta, mu_b, sigma_sq_b, mu_a, sigma_sq_a);
+
+		const alpha_1 = updated_annotator.updated_alpha;
+		const beta_1 = updated_annotator.updated_beta;
+		const c = updated_annotator.c;
+		const mu_a_1 = updated_mus.updated_mu_winner;
+		const mu_b_1 = updated_mus.updated_mu_loser;
+		const sigma_sq_a_1 = updated_sigma_sqs.updated_sigma_sq_winner;
+		const sigma_sq_b_1 = updated_sigma_sqs.updated_sigma_sq_loser;
+		const prob_a_ranked_above = c;
+		const alpha_2 = updated_annotator_2.updated_alpha;
+		const beta_2 = updated_annotator_2.updated_beta;
+		const mu_b_2 = updated_mus_2.updated_mu_winner;
+		const mu_a_2 = updated_mus_2.updated_mu_loser;
+		const sigma_sq_b_2 = updated_sigma_sqs_2.updated_sigma_sq_winner;
+		const sigma_sq_a_2 = updated_sigma_sqs_2.updated_sigma_sq_loser;
+
+		return (
+			prob_a_ranked_above * (
+				Maths.divergenceGaussian(mu_a_1, sigma_sq_a_1, mu_a, sigma_sq_a)
+				+ Maths.divergenceGaussian(mu_b_1, sigma_sq_b_1, mu_b, sigma_sq_b)
+				+ GAMMA * Maths.divergenceBeta(alpha_1, beta_1, alpha, beta)
+			) + (1 - prob_a_ranked_above) * (
+				Maths.divergenceGaussian(mu_a_2, sigma_sq_a_2, mu_a, sigma_sq_a)
+				+ Maths.divergenceGaussian(mu_b_2, sigma_sq_b_2, mu_b, sigma_sq_b)
+				+ GAMMA * Maths.divergenceBeta(alpha_2, beta_2, alpha, beta)
+			)
+		);
 	},
 
 	updatedMus: (alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser) => {
-		//TODO: Implement this
+		const mult = (alpha * exp(mu_winner)) / (alpha * exp(mu_winner) + beta * exp(mu_loser)) - (exp(mu_winner)) / (exp(mu_winner) + exp(mu_loser));
+
+		const updated_mu_winner = mu_winner + sigma_sq_winner * mult
+		const updated_mu_loser = mu_loser - sigma_sq_loser * mult
+
+		return {
+			updated_mu_winner,
+			updated_mu_loser
+		};
 	},
 
 	updatedSigmaSqs: (alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser) => {
-		//TODO: Implement this
+		const mult = (
+			(alpha * exp(mu_winner) * beta * exp(mu_loser))
+			/ Math.pow(alpha * exp(mu_winner) + beta * exp(mu_loser), 2)
+			- (exp(mu_winner) * exp(mu_loser))
+			/ Math.pow(exp(mu_winner) + exp(mu_loser), 2)
+		);
+
+		const updated_sigma_sq_winner = sigma_sq_winner * Math.max(1 + sigma_sq_winner * mult, KAPPA);
+		const updated_sigma_sq_loser = sigma_sq_loser * Math.max(1 + sigma_sq_loser * mult, KAPPA);
+
+		return {
+			updated_sigma_sq_winner,
+			updated_sigma_sq_loser
+		};
 	},
 
 	updatedAnnotator: (alpha, beta, mu_winner, sigma_sq_winner, mu_loser, sigma_sq_loser) => {
-		//TODO: Implement this
+		const c_1 = (
+			exp(mu_winner) / (exp(mu_winner) + exp(mu_loser)) + 0.5
+			* (sigma_sq_winner + sigma_sq_loser)
+			* (exp(mu_winner) * exp(mu_loser) * (exp(mu_loser) - exp(mu_winner)))
+			/ Math.pow(exp(mu_winner) + exp(mu_loser), 3)
+		);
+		const c_2 = 1 - c_1;
+		const c = (c_1 * alpha + c_2 * beta) / (alpha + beta);
+
+		const expt = (c_1 * (alpha + 1) * alpha + c_2 * alpha * beta) / (c * (alpha + beta + 1) * (alpha + beta));
+		const expt_sq = (
+			(c_1 * (alpha + 2) * (alpha + 1) * alpha + c_2 * (alpha + 1) * alpha * beta)
+			/ (c * (alpha + beta + 2) * (alpha + beta + 1) * (alpha + beta))
+		);
+
+		const variance = expt_sq - Math.pow(expt, 2)
+		const updated_alpha = ((expt - expt_sq) * expt) / variance;
+		const updated_beta = (expt - expt_sq) * (1 - expt) / variance;
+
+		return {
+			updated_alpha,
+			updated_beta,
+			c
+		};
 	},
-
-
 };
 
-module.exports = QuickMATHS;
+module.exports = Maths;
