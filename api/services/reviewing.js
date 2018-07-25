e9d2774402c3bd74558917b67af0cf491de9ff33
PatrikHolopainen <oncerecursive@gmail.com>
2018-07-25 18:33:08 +0300

    Reviewing

--- api/services/reviewing.js
+++ api/services/reviewing.js
@@ -1,44 +1,45 @@
+const mongoose = require('mongoose');
 const Promise = require('bluebird');
-
+const _ = require('lodash')
 const ReviewingService = {
 	getPreferredItems: (annotator) => {
 
-		items = [];
-		ignored_ids = annotator.ignore;
-
-
-		available_items = [];
-		// TODO judge.py rivi 162-167
-		// if (ignored_ids.length > 0) {
-		// 	Item.query.filter???
-		// }
-
-		prioritized_items = []
-		for (i = 0; i < available_items.length; i++) {
-			if (available_items[i].prioritized) {
-				prioritized_items.push(available_items[i])
-			}
-		}
-
-
-		if (prioritized.length > 0) {
-			items = prioritized_items
-		} else {
-			items = available_items
-		}
-
-
-		// TODO judge.py rivi 173-175
-
-		busy = []
-		
-
-
-
-
-
-
 		return new Promise(function (resolve, reject) {
+
+					let items = [];
+					const ignored_ids = annotator.ignore;
+					mongoose.model('Item').find({
+						active: true,
+						_id: {$nin:ignored_ids}
+					}).then((available_items) => {
+
+						const prioritized_items = []
+						for (i = 0; i < available_items.length; i++) {
+							if (available_items[i].prioritized) {
+								prioritized_items.push(available_items[i])
+							}
+						}
+
+						items = prioritized_items.length > 0 ? prioritized_items : available_items;
+
+						mongoose.model('Annotator').find({
+							active: true,
+							next: {$exists: false},
+							updated: {$exists: false},
+						}).then((annotators) => {
+							// TODO set timeout as constant
+							const nonbusy = _.filter(annotators,(annotator) => {
+								return (Date.now()-annotator.updated) > (5 * 60 * 1000)
+							})
+							const preferred = nonbusy.length > 0 ? nonbusy : items;
+
+							//TODO 178, 181
+							resolve(preferred)
+						}).catch((error) => {
+							reject(error)
+						})
+
+					});
 
 		});
 	},
