ea892465af39021a3c87ed1b3c68a918fa98de29
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-08-01 19:28:09 +0300

    improve admin test page

--- client/src/pages/Test/SubmitProjectTest/index.js
+++ client/src/pages/Test/SubmitProjectTest/index.js
@@ -10,7 +10,8 @@ class SubmitProjectTest extends Component {
 
 		this.state = {
 			teamMembers: [],
-			response: {}
+			response: {},
+			requestBody: {}
 		}
 	}
 
@@ -23,7 +24,10 @@ class SubmitProjectTest extends Component {
 
 		if (teamMember.email && teamMember.name) {
 			this.setState({
-				teamMembers: _.concat(this.state.teamMembers, teamMember)
+				requestBody: {
+					...this.state.requestBody,
+					teamMembers: this.state.requestBody.teamMembers ? _.concat(this.state.requestBody.teamMembers, teamMember) : [teamMember]
+				}
 			});
 
 			this.refs.teamMemberName.value = '';
@@ -38,10 +42,7 @@ class SubmitProjectTest extends Component {
 				'Accept': 'application/json',
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({
-				name: 'Foofoo team',
-				location: 'A5'
-			})
+			body: JSON.stringify(this.state.requestBody)
 		});
 		const body = await response.json();
 
@@ -61,15 +62,26 @@ class SubmitProjectTest extends Component {
 				<p className="Test--section-description">Test submitting a project and team members</p>
 				<div className="Test--container">
 					<p className="Test--section-description">Team details</p>
-					<input className="Test--input" placeholder="Team name"></input>
-					<input className="Test--input" placeholder="Team location"></input>
+					<input
+						className="Test--input"
+						placeholder="Team name"
+						onChange={(event) => this.setState({ requestBody: { ...this.state.requestBody, name: event.target.value } })}
+					/>
+					<input
+						className="Test--input"
+						placeholder="Team location"
+						onChange={(event) => this.setState({ requestBody: { ...this.state.requestBody, location: event.target.value } })}
+					/>
 					<p className="Test--section-description">Add team members</p>
 					<input ref="teamMemberName" className="Test--input" placeholder="Team member name"></input>
 					<input ref="teamMemberEmail" className="Test--input" placeholder="Team member email"></input>
 					<button onClick={() => this.addTeamMember()}>Add team member</button>
-					{this.renderTeamMembers()}
 				</div>
-				<button className="Test--submitButton" onClick={() => this.testRequest()}>Test</button>
+				<div className="Test--section-wrapper">
+					<h1 className="Test--section-title">Request body:</h1>
+					<ReactJson src={this.state.requestBody} />
+					<button className="Test--submitButton" onClick={() => this.testRequest()}>Test</button>
+				</div>
 				<div className="Test--section-wrapper">
 					<h1 className="Test--section-title">Response:</h1>
 					<ReactJson src={this.state.response} />
