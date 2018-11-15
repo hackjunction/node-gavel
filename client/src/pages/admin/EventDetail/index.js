77c688ff1dc49dfd7badb2eacd4130c1c183d929
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2018-11-15 12:47:36 +0200

    skeleton for admin panle

--- client/src/pages/admin/EventDetail/index.js
+++ client/src/pages/admin/EventDetail/index.js
@@ -3,14 +3,23 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './style.scss';
 
-import * as AdminActions from '../../redux/admin/actions';
-import * as admin from '../../redux/admin/selectors';
+import * as AdminActions from '../../../redux/admin/actions';
+import * as admin from '../../../redux/admin/selectors';
 
 class AdminEventDetail extends Component {
     static propTypes = {
         annotators: PropTypes.array,
         adminToken: PropTypes.string
     };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            eventId: this.props.match.params.eventId,
+            activeTab: 0
+        };
+    }
 
     componentDidMount() {
         const { adminToken, match, updateAnnotators, updateEvent, updateProjects } = this.props;
@@ -35,25 +44,72 @@ class AdminEventDetail extends Component {
     }
 
     renderResults() {
-        return null;
+        const { getProjects, isProjectsLoading, isProjectsError } = this.props;
+        const { eventId } = this.state;
+        console.log('PROJECTS', {
+            data: getProjects(eventId),
+            loading: isProjectsLoading(eventId),
+            error: isProjectsError(eventId)
+        });
+        return (
+            <React.Fragment>
+                <h4>Results</h4>
+            </React.Fragment>
+        );
     }
 
     renderAnnotators() {
-        return null;
+        const { getAnnotators, isAnnotatorsLoading, isAnnotatorsError } = this.props;
+        const { eventId } = this.state;
+        console.log('ANNOTATORS', {
+            data: getAnnotators(eventId),
+            loading: isAnnotatorsLoading(eventId),
+            error: isAnnotatorsError(eventId)
+        });
+        return (
+            <React.Fragment>
+                <h4>Annotators</h4>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        switch (this.state.activeTab) {
+            case 0:
+                return this.renderResults();
+            case 1:
+                return this.renderAnnotators();
+            case 2:
+                return null;
+            default:
+                return null;
+        }
     }
 
     render() {
-        const annotators = this.getAnnotators();
-
         return (
-            <div>
-                <h1>Admin Panel</h1>
-
-                <h4>Results</h4>
-                {this.renderResults()}
-
-                <h4>Annotators</h4>
-                {this.renderAnnotators()}
+            <div className="EventDetail">
+                <div className="EventDetail--Tabs">
+                    <div
+                        className={this.state.activeTab === 0 ? 'EventDetail--Tab active' : 'EventDetail--Tab'}
+                        onClick={() => this.setState({ activeTab: 0 })}
+                    >
+                        <span className="EventDetail--Tab_text">Results</span>
+                    </div>
+                    <div
+                        className={this.state.activeTab === 1 ? 'EventDetail--Tab active' : 'EventDetail--Tab'}
+                        onClick={() => this.setState({ activeTab: 1 })}
+                    >
+                        <span className="EventDetail--Tab_text">Annotators</span>
+                    </div>
+                    <div
+                        className={this.state.activeTab === 2 ? 'EventDetail--Tab active' : 'EventDetail--Tab'}
+                        onClick={() => this.setState({ activeTab: 2 })}
+                    >
+                        <span className="EventDetail--Tab_text">Edit</span>
+                    </div>
+                </div>
+                <div className="EventDetail--Content">{this.renderContent()}</div>
             </div>
         );
     }
@@ -61,7 +117,12 @@ class AdminEventDetail extends Component {
 
 const mapStateToProps = state => ({
     adminToken: admin.getToken(state),
-    annotators: admin.getAnnotators(state)
+    getAnnotators: admin.getAnnotators(state),
+    isAnnotatorsLoading: admin.getAnnotatorsLoading(state),
+    isAnnotatorsError: admin.getAnnotatorsError(state),
+    getProjects: admin.getProjects(state),
+    isProjectsLoading: admin.getProjectsLoading(state),
+    isProjectsError: admin.getProjectsError(state)
 });
 
 const mapDispatchToProps = dispatch => ({
--- client/src/redux/admin/selectors.js
+++ client/src/redux/admin/selectors.js
@@ -2,4 +2,44 @@ export const isLoggedIn = state => state.admin.token !== null;
 export const getToken = state => state.admin.token;
 export const getEvents = state => (state.admin.events ? state.admin.events : []);
 
-export const getAnnotators = state => (state.admin.annotators ? state.admin.annotators : {});
+export const getAnnotators = state => eventId => {
+    if (state.admin.annotators && state.admin.annotators.hasOwnProperty(eventId)) {
+        return state.admin.annotators[eventId].data;
+    }
+    return [];
+};
+
+export const getAnnotatorsLoading = state => eventId => {
+    if (state.admin.annotators && state.admin.annotators.hasOwnProperty(eventId)) {
+        return state.admin.annotators[eventId].loading;
+    }
+    return false;
+};
+
+export const getAnnotatorsError = state => eventId => {
+    if (state.admin.annotators && state.admin.annotators.hasOwnProperty(eventId)) {
+        return state.admin.annotators[eventId].error;
+    }
+    return false;
+};
+
+export const getProjects = state => eventId => {
+    if (state.admin.projects && state.admin.projects.hasOwnProperty(eventId)) {
+        return state.admin.projects[eventId].data;
+    }
+    return [];
+};
+
+export const getProjectsLoading = state => eventId => {
+    if (state.admin.projects && state.admin.projects.hasOwnProperty(eventId)) {
+        return state.admin.projects[eventId].loading;
+    }
+    return false;
+};
+
+export const getProjectsError = state => eventId => {
+    if (state.admin.projects && state.admin.projects.hasOwnProperty(eventId)) {
+        return state.admin.projects[eventId].error;
+    }
+    return false;
+};
