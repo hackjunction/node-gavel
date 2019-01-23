ccd94a8f050bd86001fc4fc20e9ea5f52413fbe3
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-01-23 15:40:32 +0200

    Add participant data export functionality to admin section

--- client/src/pages/admin/EventDetail/OverviewTab/index.js
+++ client/src/pages/admin/EventDetail/OverviewTab/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import moment from 'moment-timezone';
+import _ from 'lodash';
+import { CSVLink } from 'react-csv';
 
 import './style.scss';
 import * as AdminSelectors from '../../../../redux/admin/selectors';
 import * as AdminActions from '../../../../redux/admin/actions';
-
+import API from '../../../../services/api';
 import BannerManager from '../../../../components/BannerManager';
 import SettingsItem from './SettingsItem';
 
@@ -24,11 +26,27 @@ class OverviewTab extends Component {
 
         this.state = {
             trackWinnersPublic: false,
-        }
+            challenges: null,
+            winners: null,
+            exportData: null,
+        };
 
         this.extendSubmissionDeadline = this.extendSubmissionDeadline.bind(this);
         this.toggleTrackWinnersPublic = this.toggleTrackWinnersPublic.bind(this);
         this.toggleFinalistVotingOpen = this.toggleFinalistVotingOpen.bind(this);
+        this.exportData = this.exportData.bind(this);
+    }
+
+    componentDidMount() {
+        const { adminToken, event } = this.props;
+
+        API.adminGetChallengesForEvent(adminToken, event._id).then(challenges => {
+            this.setState({ challenges });
+        });
+
+        API.adminGetChallengeWinnersForEvent(adminToken, event._id).then(winners => {
+            this.setState({ winners });
+        })
     }
 
     toggleTrackWinnersPublic() {
@@ -87,8 +105,88 @@ class OverviewTab extends Component {
         });
     }
 
+    exportData() {
+        const { annotators, projects } = this.props;
+
+        const byTrack = {};
+
+        _.each(projects, p => {
+            if (byTrack.hasOwnProperty(p.track)) {
+                byTrack[p.track].push(p);
+            } else {
+                byTrack[p.track] = [p];
+            }
+        });
+
+        _.forOwn(byTrack, (value, key) => {
+            byTrack[key] = _.reverse(_.sortBy(value, 'mu'));
+        });
+
+        const data = _.map(annotators, a => {
+            const project = _.find(projects, p => {
+                return p.team === a.team;
+            });
+
+            let challenges = [];
+
+            if (project) {
+                challenges = _.map(project.challenges, c => {
+                    let chall = _.find(this.props.event.challenges, ch => {
+                        return ch._id.toString() === c.toString();
+                    });
+
+                    if (chall) {
+                        let challWinners = _.find(this.state.winners, w => {
+                            return w.challenge.toString() === chall._id.toString();
+                        });
+
+                        let placement = 'No prize';
+
+                        if (challWinners) {
+                            const isFirst = challWinners.first.toString() === project._id.toString();
+                            const isSecond = !isFirst && challWinners.second && challWinners.second.toString() === project._id.toString();
+                            const isThird = !isSecond && challWinners.third && challWinners.third.toString() === project._id.toString();
+
+                            if (isFirst) {
+                                placement = '1st';
+                            } else if (isSecond) {
+                                placement = '2nd';
+                            } else if (isThird) {
+                                placement = '3rd';
+                            }
+                        }
+
+                        return chall.name + ' by ' + chall.partner + ' (' + placement + ')';
+                    } else {
+                        return '';
+                    }
+                });
+                console.log('CHALLENGES', challenges);
+            }
+
+            const track = project ? _.find(this.props.event.tracks, t => t._id === project.track) : null;
+
+            return {
+                'Name': a.name,
+                'Email': a.email,
+                'Project Name': project ? project.name : '',
+                'Project Description': project ? project.description : '',
+                'Track Name': track ? track.name : 'N/A',
+                'Track Placement': project ? _.indexOf(byTrack[project.track], project) + 1 : 'N/A',
+                'Challenges': project ? challenges.join(', ') : 'N/A',
+            }
+        });
+
+        this.setState({
+            exportData: data
+        });
+
+        return Promise.resolve();
+    }
+
     render() {
         const { adminToken, eventId } = this.props;
+        const { challenges, winners, exportData } = this.state;
         const { track_winners_public, finalist_voting_open } = this.props.event;
 
         return (
@@ -117,6 +215,26 @@ class OverviewTab extends Component {
                         buttonText={!finalist_voting_open ? "Open voting" : "Close voting"}
                         onClick={this.toggleFinalistVotingOpen}
                     />
+                    {challenges && winners ? (
+                        <SettingsItem
+                            title={'Generate participant report'}
+                            desc={'Generate a .csv with participants and their projects + placements'}
+                            isPositive={true}
+                            buttonText={'Generate'}
+                            onClick={this.exportData}
+                        />
+                    ) : null}
+                    {exportData ? (
+                        <CSVLink
+                            data={this.state.exportData}
+                            filename={"participant-report.csv"}
+                            className="btn btn-primary mt-2"
+                            target="_blank"
+                            separator=";"
+                        >
+                            Download report
+                        </CSVLink>
+                    ) : null}
                 </div>
             </React.Fragment>
         );
